f2ea0657413352503799d4bd6e0af9e4519c6788
pijusjalo-source <pijusjalo@gmail.com>
2026-09-21 18:49:57 +0300

    Update home route to include body styling
    Change the home route to return a styled HTML body.

--- app.py
+++ app.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     greeting = os.environ.get("GREETING", "Hello from the cloud!")
-    return f"<p>{greeting}</p><p>App developed by Pijus</p><p>Please use responsibly</p>"
-    
+    return (
+        '<body style="background-color: yellow;">'
+        f"<p>{greeting}</p>"
+        "<p>App developed by Pijus</p>"
+        "<p>Please use responsibly</p>"
+        "</body>"
+    )
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
