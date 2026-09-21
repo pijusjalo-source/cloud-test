eb5f0874ccbdb765d5719bc33f04d3326b820c34
pijusjalo-source <pijusjalo@gmail.com>
2026-09-21 14:57:29 +0300

    Initialize Flask app with greeting route

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    greeting = os.environ.get("GREETING", "Hello from the cloud!")
+    return greeting
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
